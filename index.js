6b173b6237db7e04d04445afdc040f8d60825b6e
Sugut <sugutvictorine@gmail.com>
2022-08-18 09:38:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(work = "roller-skate") {
+    return `This Saturday, I want to ${work}!`;
+}
+    
+
+function mondayWork(fun ="go to the office"){
+    return `This Monday, I will ${fun}.`
+}
+
+let wrapAdjective= function(msg1="*"){
+    return function (adjective = "special"){
+        return( `You are ${msg1}${adjective}${msg1}!`);
+    }
+}    
+wrapAdjective("%")("a dedicated programmer!");
